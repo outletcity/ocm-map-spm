53ee4eb4152f9ad0ed0d5d91f40c3852ea6616ca
Andreas Bruns <andreas.bruns@outletcity.com>
2025-03-25 14:10:32 +0100

    initial commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let unityFrameworkXCFramework = Target.binaryTarget(
+  name: "UnityFramework",
+  url: "https://github.com/outletcity/ocm-map-spm/releases/download/3.28.1/UnityFramework.xcframework.zip",
+  checksum: "e5e8a3b50d451c9bf4038acddc8ceaa24e9e9449eb610cd73d59798590f9e3f2"
+)
+
+let package = Package(
+    name: "UnityMapFramework",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(
+            name: "UnityMapFramework",
+            targets: ["UnityFramework", "UnityFrameworkStub"])
+    ],
+    targets: [
+        unityFrameworkXCFramework,
+
+        // Without at least one regular (non-binary) target, this package doesn't show up
+        // in Xcode under "Frameworks, Libraries, and Embedded Content". That prevents
+        // LinkKit from being embedded in the app product, causing the app to crash when
+        // ran on a physical device. As a workaround, we can include a stub target
+        // with at least one source file.
+        // https://github.com/apple/swift-package-manager/issues/6069
+        .target(name: "UnityFrameworkStub", path: "Sources", resources: [.copy("Resources/PrivacyInfo.xcprivacy")]),
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ocm-map-spm
+
 Provides Swift Package Manager support for ocm-map
+This repository provides Swift Package Manager support for ocm-map, allowing to easily integrate the MapFramework into iOS applications.
+
+## Why is there a separate repository for Swift Package Manager support?
+
+The generated files are to large to be hosted as sourcecode files (> 100 MB) and Swift Package Manger does not support LFS. Therefore this repository contains a pointer the the precompiled XCFramework.
